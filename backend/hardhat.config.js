27b959682830d92e9d2067fa47e0024d0c8e0234
RonexLemon <ronexondimu@gmail.com>
2023-03-12 09:52:09 +0300

    configure hardhat

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config({path:".env"});
+KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
   solidity: "0.8.18",
+  networks:{
+    mantle:{
+      url: "https://rpc.testnet.mantle.xyz",
+      chainId: 5001,
+      accounts: [KEY],
+    }
+  }
 };

ff9788e871b0b283359c8840293fe3d6214866a8
RonexLemon <ronexondimu@gmail.com>
2023-03-13 22:25:19 +0300

    deploy contract

--- backend/scripts/blockBank.js
+++ backend/scripts/blockBank.js
@@ -1,4 +1,7 @@
 const {ethers} = require("hardhat");
+//GoldTokenContractAddress 0x1D7478635b1e6C0001432a5a7e20Fd273273Aa32
+//SilverokenContractAddress 0x8831Dc4233fd71E07de7eD6329e3D1f402368d83
+//FarmContractAddress 0x1E4D739690aBa61e9FC02fA638b1747855c1e144
 
 async function main(){
     //get the contracts

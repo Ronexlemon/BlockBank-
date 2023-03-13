0cefe9edbd066221718787302587534848b5022f
RonexLemon <ronexondimu@gmail.com>
2023-03-13 23:40:11 +0300

    add contract addresses

---
+++ client/src/contractaddress/exportaddress.js
@@ -0,0 +1,6 @@
+//GoldTokenContractAddress 0x1D7478635b1e6C0001432a5a7e20Fd273273Aa32
+//SilverokenContractAddress 0x8831Dc4233fd71E07de7eD6329e3D1f402368d83
+//FarmContractAddress 0x1E4D739690aBa61e9FC02fA638b1747855c1e144
+export const GoldTokenContractAddress = "0x1D7478635b1e6C0001432a5a7e20Fd273273Aa32"
+export const SilverokenContractAddress = "0x8831Dc4233fd71E07de7eD6329e3D1f402368d83"
+export const  FarmContractAddress = "0x1E4D739690aBa61e9FC02fA638b1747855c1e144"

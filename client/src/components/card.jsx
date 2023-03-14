8b375490af0f744c8dc8f92d48c8b2d7a8114e03
RonexLemon <ronexondimu@gmail.com>
2023-03-14 06:58:15 +0300

    implement swap function bit-gold,bit-silver

--- client/src/components/card.jsx
+++ client/src/components/card.jsx
@@ -16,34 +16,75 @@ import { ethers } from "ethers";
 
 const Card = () =>{
   const [amount, setAmount] = useState('');
+  const [choice, setChoice] = useState(0);
+  const [choiceto, setChoiceto] = useState(0);
+  const [symbolFrom, setSymbolFrom] = React.useState(0);
+  const [symbolTo, setSymbolTO] = React.useState(0);
+  const cryptos =[
+      'Mantle Token','Gold Token','Silver Token'
+  ]
+  const options =[
+    0,1,2 , //BIT , Gold Silver
+  ]
+  const optionsto =[
+    0,1,2 //silver , gold, BIT
+  ]
+    
+  
+  
   const handleInputChange = (event) => {
     setAmount(event.target.value);
+  }
+  const handleOptionChange = (event) => {
+    setSymbolFrom(event.target.value)
+     setChoice(options[event.target.value])
+  }
+  const handleOptionChangeTo = (event) => {
+    setSymbolTO(event.target.value) 
+     setChoiceto(optionsto[event.target.value])
   }
   const {
     getProviderOrSigner,
     connected,
     Contract,
 } = useContext(AppContext)
-  const swapBITforGoldToken = async()=>{
+  const swapBITforTokens = async(from, to)=>{
     try{
       const valueInWei = ethers.utils.parseEther(amount); 
-      console.log("rthers are",valueInWei)
-        const signer = await getProviderOrSigner(true);
-        const farmContract = new Contract(FarmContractAddress,FarmAbi,signer);
-       const transaction =  await farmContract.buyGoldToken(10,{value: valueInWei});
+      const tokenamount = parseInt(amount)*10
+      const signer = await getProviderOrSigner(true);
+      const farmContract = new Contract(FarmContractAddress,FarmAbi,signer);
+      if(from == options[0] && to == optionsto[1]){ //swaping BIT  for gold
+        const transaction =  await farmContract.buyGoldToken(tokenamount,{value: valueInWei});
+        await  transaction.wait();
+       
+
+      }
+      else if(from == options[0] && to == optionsto[0] ){ //swaping BIT for silver
+        const transaction =  await farmContract.buySilverToken(tokenamount,{value: valueInWei});
         await  transaction.wait();
 
+      }
+      else if(from == options[1] && to == optionsto[0] ){ //swaping gold for silver
+        const transaction =  await farmContract.swapGoldTokensForSilver(parseInt(amount));
+        await  transaction.wait();
+
+      }
+
+      else{
+        alert("please select an option")
+      }
+     
+        
+       
+       
 
     }catch(error){
         console.log("failed to swap BIT for  gold token",error);
     }
 }
    
-    const [symbolFrom, setSymbolFrom] = React.useState(0);
-    const [symbolTo, setSymbolTO] = React.useState(0);
-    const cryptos =[
-        'Mantle Token','Gold Token','Silver Token'
-    ]
+   
     const defaultCryptos = cryptos[0];
     return(
         <div className="h-3/4 w-3/4 mt-10  bg-black rounded-2xl  text-white">
@@ -52,12 +93,12 @@ const Card = () =>{
                   <div className="flex justify-between  items-center m-4 ">
                     <div>
                         
-                    <h5>From : {(symbolFrom == 0)?cryptos[0] :(symbolFrom ==1)?cryptos[1]: cryptos[2] }</h5>
+                    <h5>From : {(symbolFrom == 0)?cryptos[0]   :(symbolFrom ==1)?cryptos[1] : cryptos[2] }</h5>
                     </div>
                     <div className="flex justify-end items-center gap-2">
 <span className="h-5 w-5"> {(symbolFrom == 0)?<img src={ethereum} alt="symbol"/>:(symbolFrom ==1)?<img src={gold} alt="symbol"/>: <img src={silver} alt="symbol"/> } </span>
 
-<span><select className="bg-black text-white" value={symbolFrom} onChange={(event) => setSymbolFrom(event.target.value)}>
+<span><select className="bg-black text-white" value={symbolFrom} onChange={handleOptionChange}>
 <option value={0}>BIT</option>
  <option value={1}>GLD</option>
 <option value={2}>SLV</option>
@@ -83,12 +124,12 @@ const Card = () =>{
                 <div className=" text-sm  text-gray-400 rounded-2xl grid grid-row-4  ">
                   <div className="flex justify-between  items-center m-4 ">
                     <div>
-                    <h5>To : {(symbolTo == 0)?cryptos[2] :(symbolTo ==1)?cryptos[1]: cryptos[0] }</h5>
+                    <h5>To : {(symbolTo == 0)?cryptos[2] /**silver */ :(symbolTo ==1)?cryptos[1]  /** gold */: cryptos[0]  }</h5>
                     </div>
                     <div className="flex justify-end items-center gap-2">
                     <span className="h-5 w-5"> {(symbolTo == 0)?<img src={silver} alt="symbol"/>:(symbolTo ==1)?<img src={gold} alt="symbol"/>: <img src={ethereum} alt="symbol"/> } </span>
 
-<span><select className="bg-black text-white" value={symbolTo} onChange={(event) => setSymbolTO(event.target.value)}>
+<span><select className="bg-black text-white" value={symbolTo} onChange={handleOptionChangeTo}>
 
 <option value={2}>BIT</option> 
 <option value={1}>GLD</option>
@@ -105,11 +146,13 @@ const Card = () =>{
                   <div className="flex justify-start gap-2">
                     <h5>Balance</h5>
                     <span>79</span>
+                    {console.log("otion from",choice)}
+                    {console.log("otion to",choiceto)}
                     
                   </div>
                   </div>
                   <div className="text-white ml-4 mr-4">
-                    <button onClick={()=>{swapBITforGoldToken()}} className="w-full bg-blue-500 rounded  ">Transfer</button>
+                    <button onClick={()=>{swapBITforTokens(choice,choiceto)}} className="w-full bg-blue-500 rounded  ">Transfer</button>
 
                 </div>
 
